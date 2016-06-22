126fc8b62275b164aac62fa4f296c30fb1b12a26
oflucas <scofieldgood@gmail.com>
2016-06-22 10:46:25 +0800

    Create 278_First_Bad_Version.java

---
+++ BinarySearch/278_First_Bad_Version.java
@@ -0,0 +1,14 @@
+//18ms 50% Total Accepted: 48586 Total Submissions: 213504 Difficulty: Easy
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int i = 1, j = n;
+        while (i < j) {
+            int mid = i + (j - i) / 2;
+            if (isBadVersion(mid)) j = mid; else i = mid + 1;
+        }
+        return i;
+    }
+}
